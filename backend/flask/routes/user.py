beac9d766f015ae9e817412266a73aa6177a8811
Lainneker-kairos <lainneker@gmail.com>
2026-06-30 12:07:56 +0200

    conexion de usuario con ambos perfiles

--- backend/flask/routes/user.py
+++ backend/flask/routes/user.py
@@ -158,7 +158,7 @@ def bepetsitter(current_user_id):
     city = data.get('city') or (user.owner_profile.city if user.owner_profile else "")
 
     # Actualizar el rol del usuario
-    user.role = 'petsitter',  # Ahora el usuario puede ser dueño y cuidador
+    user.role = 'petsitter'  # Ahora el usuario puede ser dueño y cuidador
 
     # Crear el nuevo perfil de cuidador
     new_profile = Petsitter(
