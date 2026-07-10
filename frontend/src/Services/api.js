9091e4a7f0792e9845069cceab8241c7adcc4802
Lainneker-kairos <lainneker@gmail.com>
2026-07-10 21:57:57 +0200

    solucion frontend

--- frontend/src/Services/api.js
+++ frontend/src/Services/api.js
@@ -1,6 +1,6 @@
 "use client"
 
-const API_URL = "http://127.0.0.1:5000/api"
+const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://127.0.0.1:5000/api";
 
 //-- Función para detectar el rol del Usuario desde el Token --//
 export const getUserRoleFromToken = () => {
